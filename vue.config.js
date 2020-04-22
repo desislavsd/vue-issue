bb364eba1417bf72b37922943af86a9c4c482588
desislavsd <n.nosme@gmail.com>
2020-04-22 10:18:36 +0300

    fixed vue.config in dev

--- vue.config.js
+++ vue.config.js
@@ -2,15 +2,18 @@ module.exports = {
 
     chainWebpack(config){
 
+        if(process.env.NODE_ENV == 'development') return;
+
         config.plugin('optimize-css').tap( args => {
+
 
             // nanocss causes problems with postcss when `calc` is minified
             Object.assign(args[0].cssnanoOptions.preset[1], {
                 'calc': false,
             });
-
+            
             return args;
-
+            
         })
     }
 }
